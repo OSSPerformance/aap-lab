0241992427e86330f033bfa18ed718ef2220c870
Manos Aggelou <manos05@gmail.com>
2026-02-05 17:14:36 +0200

    adder reboot

---
+++ application/tests/test_app.py
@@ -0,0 +1,10 @@
+from app import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-1, -2) == -3
+
+def test_add_mixed_numbers():
+    assert add(-1, 5) == 4
